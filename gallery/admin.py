fca99b5f9693caafc242e938b90d862f92bb6740
aluoch-sheila <aluochsheila1999@gmail.com>
2018-12-08 19:41:08 +0300

    imported admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 from django.contrib import admin
 
 # Register your models here.
+from .models import Posts,Category,Location
+
+class AdminCat(admin.ModelAdmin):
+    filter_horizontal=('category',)
+
+admin.site.register(Posts,AdminCat)
+admin.site.register(Category)
+admin.site.register(Location)

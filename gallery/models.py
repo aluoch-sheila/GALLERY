689073baed4cffe92d2deca855f8eb29dbdf485e
aluoch-sheila <aluochsheila1999@gmail.com>
2018-12-08 20:25:09 +0300

    imported models

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,46 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.location
+   
+
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+
+    
+    def __str__(self):
+        return self.name
+
+
+class Posts(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    article_image = models.ImageField(upload_to = 'articles/')
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering=['-id']
+    
+    @classmethod
+    def save_post(self):
+        self.save()
+    @classmethod
+    def get_posts(cls):
+         posts = cls.objects.all()
+         return posts
+    @classmethod
+    def search_by_category(cls,search_term):
+        images = cls.objects.filter(category__name__icontains=search_term)
+        return images
+    def delete_post(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.name
---
+++ gallery/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    url(r'^pic/(\d+)',views.full_pic,name='pic'),
+    url(r'^location/(\d+)',views.locations,name='locations'),
+    url(r'^search',views.search_results,name='search_results'),
+]
+if settings.DEBUG:
+     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
